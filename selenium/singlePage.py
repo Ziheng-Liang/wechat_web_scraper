0ba0d67ba66efac1dcd035a155e59ea994252b6e
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-07-05 10:10:57 -0400

    Finish scraping all nested text with beautifulsoup

--- selenium/singlePage.py
+++ selenium/singlePage.py
@@ -1,8 +1,44 @@
-from lxml import html
-import requests
+import urllib
+from bs4 import BeautifulSoup
+import re
+
+html = urllib.urlopen('http://mp.weixin.qq.com/s?__biz=MjM5MjAyOTEzMg==&mid=2650157004&idx=1&sn=3a85bd6c678f00b2cdbe73b80e964d60&scene=19#wechat_redirect').read()
+soup = BeautifulSoup(html, 'html.parser')
+texts = soup.findAll(text=True)
+
+def visible(element):
+    if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+        return False
+    elif re.match('<!--.*-->', element.encode('utf-8')):
+        return False
+    return True
+
+visible_texts = filter(visible, texts)
+
+# for visible_text in visible_texts:
+# 	print visible_text.encode('utf-8')
+
+def removeSpace(string):
+	return string.replace("\n", "")
+
+combined_string = ''.join(map(removeSpace, visible_texts))
+
+print combined_string
 
 
-page = requests.get('http://mp.weixin.qq.com/s?__biz=MjM5MjAyOTEzMg==&mid=2650157004&idx=1&sn=3a85bd6c678f00b2cdbe73b80e964d60&scene=19#wechat_redirect')
-tree = html.fromstring(page.content)
 
-print page.content
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
