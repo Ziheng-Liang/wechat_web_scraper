1b94b62e915327940a72869bb059edb07fb451c5
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-07-06 17:19:02 -0400

    Add a web client that allow user to upload text to backend

---
+++ web client/compute_input.py
@@ -0,0 +1,28 @@
+## compute_input.py
+
+import sys, json, numpy as np
+from textblob.blob import TextBlob
+
+#Read data from stdin
+def read_in():
+    lines = sys.stdin.readlines()
+    #Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
+
+def main():
+    #get our data as an array from read_in()
+    lines = read_in()
+    chinese_blob = TextBlob(lines)
+    #create a numpy array
+    # np_lines = np.array(lines)
+
+    #use numpys sum method to find sum of all elements in the array
+    # lines_sum = np.sum(np_lines)
+
+    #return the sum to the output stream
+    # print lines_sum
+    print lines
+
+#start process
+if __name__ == '__main__':
+    main()
---
+++ web client/serve.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app     = express();
+
+//Note that in version 4 of express, express.bodyParser() was
+//deprecated in favor of a separate 'body-parser' module.
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//app.use(express.bodyParser());
+
+
+app.post('/ada', function(req, res) {
+	//start.js
+	var spawn = require('child_process').spawn,
+	    py    = spawn('python', ['compute_input.py']),
+	    // data = [1,2,3,4,5,6,7,8,9],
+	    data = req.body.description
+	    dataString = '';
+
+	py.stdout.on('data', function(data){
+	  dataString += data.toString();
+	});
+	py.stdout.on('end', function(){
+	  console.log('Sum of numbers=',dataString);
+	});
+	py.stdin.write(JSON.stringify(data));
+	py.stdin.end();
+
+  res.send('You sent the name "' + req.body.name + '". ' + req.body.description + " " + dataString);
+});
+
+app.listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
+});
