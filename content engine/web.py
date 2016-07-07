566d2d295a8f3f857384c0ba3e0efd4ee5fda807
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-07-06 22:52:33 -0400

    Finish flask \update() functionality to update csv file with UGC

--- content engine/web.py
+++ content engine/web.py
@@ -1,6 +1,7 @@
 from flask.ext.api import FlaskAPI
 from flask import request, current_app, abort
 from functools import wraps
+import csv
 
 app = FlaskAPI(__name__)
 app.config.from_object('settings')
@@ -35,7 +36,50 @@ def train():
     content_engine.train(data_url)
     return {"message": "Success!", "success": 1}
 
+# note that backup.csv has fields: [id,title,author,date,content].
+@app.route('/update')
+@token_auth
+def update():
+    title = request.data.get('title')
+    author = request.data.get('author')
+    date = request.data.get('date')
+    content = request.data.get('content')
+    if content and len(content) > 100:
+        with open('backup.csv') as source:
+            reader = csv.DictReader(source.read().splitlines())
+            # return "number of row: " + str(len(list(reader))) # return the number of rows inside backup.csv, used as next index.
+            rowid = str(len(list(reader)))
+            newrow = [rowid, title, author, date, content]
+            with open('backup.csv', 'a') as target:
+                writer = csv.writer(target)
+                writer.writerow(newrow)
+                return newrow
+    else:
+        return "Just a reminder that it's successfully updated, while it won't modify the database for now."
+
+
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
